0b6bc12bda2610e3e222c9a3eb141d87509fe7e2
Jason <jxieeducation@berkeley.edu>
2015-06-07 15:48:12 -0700

    better nodes description

--- v0/schema/initialize.js
+++ v0/schema/initialize.js
@@ -3,5 +3,5 @@ var schema = require('./schema.js');
 var nodeLib = new schema.Lib({name:"nodejs", description:"a web framework"});
 nodeLib.save(function (err) {if (err) console.log ('Error on save!')});
 
-var nodeReview1 = new schema.Review({lib: nodeLib, stars: 3, content: "LOLLLL"});
+var nodeReview1 = new schema.Review({lib: nodeLib, stars: 3, content: "super easy db migrations, but mongo's call backs can make code look really messy."});
 nodeReview1.save(function (err) {if (err) console.log ('Error on save!')});
